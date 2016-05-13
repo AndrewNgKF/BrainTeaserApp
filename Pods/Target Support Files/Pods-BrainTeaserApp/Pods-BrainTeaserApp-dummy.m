c6abb4956249c5ea52f1b6b8f66f319f47665eaa
AndrewNgKF <andrew.ctrl.alt.del@gmail.com>
2016-05-13 22:45:18 +0800

    Initial Commit after adding main login

---
+++ Pods/Target Support Files/Pods-BrainTeaserApp/Pods-BrainTeaserApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BrainTeaserApp : NSObject
+@end
+@implementation PodsDummy_Pods_BrainTeaserApp
+@end
---
+++ Pods/Target Support Files/Pods-BrainTeaserApp/Pods-BrainTeaserApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BrainTeaserAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BrainTeaserAppVersionString[];
+
